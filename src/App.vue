4564e706269034fdbede7bf4bb6194286aab0b26
Belgacem-Dahmen <belgacem.dahmen@gmail.comn>
2023-02-17 16:44:47 +0100

    vision-360

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import {  RouterView } from 'vue-router'
+import Nav from "./components/Nav.vue"
+import Sidebar from './components/Sidebar.vue';
+</script>
+
+<template>
+  
+    
+      <Nav />
+    <Sidebar />
+  <div class="ml-64 p-24 h-80 rounded-md">
+  <RouterView />
+  </div>
+    
+</template>
+
+<style scoped>
+ 
+</style>
